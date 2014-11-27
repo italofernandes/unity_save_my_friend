fda36a1c6ebafbe05ddd97ee709cd6bb5a594fa1
Ítalo <italo.fernandes@tap4mobile.com.br>
2014-11-27 08:35:45 -0400

    added dood trap implementation

---
+++ Assets/DoorTrapScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrapScript : MonoBehaviour {
+
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag.Equals("Hero23")) {
+			//Refactor to Animation;
+			door.SetActive(false);
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag.Equals("Hero23")) {
+			//Refactor to Animation;
+			door.SetActive(true);
+		}
+	}
+}
